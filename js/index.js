99cb4e0a918872ff68a57657ab2e0fc82832657a
Marichka <svitlananester@ukr.net>
2017-09-30 11:21:41 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('form input[type=submit]').click(function(e){
+        if(!$('form')[0].checkValidity()){
+             return;
+        }
+        e.preventDefault();
+         $.ajax({
+            url: "https://formspree.io/marichkanester@gmail.com",
+            method: "POST",
+            data: { 
+                personName: $('#inp-name').val(),
+                personEmail: $('#inp-email').val(),
+                message: $('#your-message').val()},
+            dataType: "json"
+        }).done(function(){
+            $('form')[0].reset();
+        $('#message-box').html('Thank you!');
+        }).fail(function() {
+            $('#message-box').html('Error');
+        });
+    })
+    
+})
