034b3fe613f8ec8438348255fe7431f728f58e2c
Marichka <svitlananester@ukr.net>
2017-10-06 15:40:16 +0000

    Initial

--- js/photo.js
+++ js/photo.js
@@ -1,20 +1,20 @@
 var i = 0;
-var image = document.getElementById("image");
-var imgs = ["my-photo/evening-rome.jpg", "my-photo/france.jpg", "my-photo/genova-water.jpg", "my-photo/genova.jpg", "my-photo/italy-sun.jpg", "my-photo/louvre.jpg", "my-photo/pragha.jpg", "my-photo/rome.jpg", "my-photo/venecia.jpg"];
+var imageM = document.getElementById("image");
+var imgsM = ["my-photo/evening-rome.jpg", "my-photo/france.jpg", "my-photo/genova-water.jpg", "my-photo/genova.jpg", "my-photo/italy-sun.jpg", "my-photo/louvre.jpg", "my-photo/pragha.jpg", "my-photo/rome.jpg", "my-photo/venecia.jpg"];
 document.getElementById('arrow-left').onclick = function() {
   i--;
-  image.src = imgs[i];
+  imageM.src = imgsM[i];
   if (i <= 0) {
-    image.src = imgs[0];
+    imageM.src = imgsM[0];
     i = 0;
   }
 }
 document.getElementById('arrow-right').onclick = function() {
   i++;
-  image.src = imgs[i];
+  imageM.src = imgsM[i];
 
-  if (i >= imgs.length) {
-    image.src = imgs[0];
+  if (i >= imgsM.length) {
+    imageM.src = imgsM[0];
     i = 0;
   }
 }
